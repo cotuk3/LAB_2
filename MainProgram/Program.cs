8b69835ae0bbe9b16660c0cd609b84e5c984a63a
cotuk3 <bohdanliashenko2004@gmail.com>
2022-09-05 12:54:50 +0300

    Add project files.

---
+++ MainProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using My_String;
+
+namespace MainProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyString my = new MyString("Hello");
+            Console.WriteLine(my);
+
+            my.InsertSubString("aaa", 2);
+
+            Console.WriteLine(my);
+
+            Console.Read();
+        }
+    }
+}
---
+++ MyString/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace My_String
+{
+    public class MyString
+    {
+        public MyString(string value)
+        {
+            Value = value;
+        }
+
+        public string Value { get; private set; }
+        public int Length
+        {
+            get
+            {
+                return Value.Length;
+            }
+        }
+
+        public bool IsSubString(string subString)
+        {
+
+
+            return false;
+        }
+
+        public void InsertSubString(string subString, int index)
+        {
+            if (index <= Value.Length)
+            {
+                char[] res = new char[Value.Length + subString.Length];
+                char[] chars = Value.ToCharArray();
+
+                int i = 0;
+                for (; i < index; i++)
+                    res[i] = chars[i];
+
+                for (int j = 0; j < index; j++, i++)
+                    res[i] = subString.ToCharArray()[j];
+
+                for (int j = index; j < res.Length; j++, i++)
+                    res[i] = chars[i];
+
+                Value = res.ToString();
+            }
+        }
+
+        public void ChangeSubString(string subString, string newSubString)
+        {
+            if (IsSubString(subString))
+            {
+
+            }
+            else
+                throw new ArgumentException($"this string: {Value} does not contain subString: {subString}");
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+
+    }
+}
