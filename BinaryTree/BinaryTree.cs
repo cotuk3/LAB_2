81c5cfd8ba1d191682325ed8cf4c7d63e76f1a1f
cotuk3 <bohdanliashenko2004@gmail.com>
2022-09-11 09:48:33 +0300

    added 3 traversals

--- BinaryTree/BinaryTree.cs
+++ BinaryTree/BinaryTree.cs
@@ -176,6 +176,7 @@ namespace MyBinaryTree
         #region IEnumerator<T>
         public IEnumerator<T> GetEnumerator()
         {
+            //return PreOrderTraversal().GetEnumerator();
             return PostOrderTraversal().GetEnumerator();
         }
 
@@ -183,11 +184,9 @@ namespace MyBinaryTree
         {
             return GetEnumerator();
         }
-
-
         #endregion
 
-        #region Post Order Traversal
+        #region Post Order Traversal (left right root)
         public List<T> PostOrderTraversal()
         {
             List<T> list = new List<T>();
@@ -208,8 +207,53 @@ namespace MyBinaryTree
 
             return list;
         }
+        #endregion 
+
+        #region Pre Order Traversal (root left right)
+        public List<T> PreOrderTraversal()
+        {
+            List<T> list = new List<T>();
+            return PreOrderTraversal(root, ref list);
+        }
+        List<T> PreOrderTraversal(BinaryTreeNode<T> root, ref List<T> list)
+        {
+            if (root == null)
+                throw new Exception("Tree is empty");
+
+            list.Add(root.Value);
+
+            if (root.Left != null)
+                PreOrderTraversal(root.Left, ref list);
+
+            if (root.Right != null)
+                PreOrderTraversal(root.Right, ref list);
+
+            return list;
+        }
+        #endregion 
+
+        #region In Order Traversal (left root right)
+        public List<T> InOrderTraversal()
+        {
+            List<T> list = new List<T>();
+            return InOrderTraversal(root, ref list);
+        }
+        List<T> InOrderTraversal(BinaryTreeNode<T> root, ref List<T> list)
+        {
+            if (root == null)
+                throw new Exception("Tree is empty");
+
+            if (root.Left != null)
+                InOrderTraversal(root.Left, ref list);
+
+            list.Add(root.Value);
+
+            if (root.Right != null)
+                InOrderTraversal(root.Right, ref list);
+
+            return list;
+        }
 
         #endregion
-
     }
 }
